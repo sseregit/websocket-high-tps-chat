9cd306db2dc4b848d9ec673ec964d22e82535c1f
장연식 <sseregit@knowwheresoft.com>
2025-03-31 16:07:39 +0900

    websockets통신에 대한 연결 및 끊김 처리 로직 작성하기

--- network/socket.go
+++ network/socket.go
@@ -19,13 +19,13 @@ type message struct {
 
 type Room struct {
 	Forward chan *message // 수신되는 메시지 보관, 들어오는 메시지를 다른 클라이언트들에게 전송
-	Join    chan *Client  // Socket이 연결되는 경우에 작동
-	Leave   chan *Client  // Socket이 끊어지는 경우에 대해서 작동
+	Join    chan *client  // Socket이 연결되는 경우에 작동
+	Leave   chan *client  // Socket이 끊어지는 경우에 대해서 작동
 
-	Clients map[*Client]bool // 현재 방에 있는 Client 정보를 저장
+	clients map[*client]bool // 현재 방에 있는 client 정보를 저장
 }
 
-type Client struct {
+type client struct {
 	Send   chan *message
 	Room   *Room
 	Name   string
@@ -35,9 +35,9 @@ type Client struct {
 func NewRoom() *Room {
 	return &Room{
 		Forward: make(chan *message),
-		Join:    make(chan *Client),
-		Leave:   make(chan *Client),
-		Clients: make(map[*Client]bool),
+		Join:    make(chan *client),
+		Leave:   make(chan *client),
+		clients: make(map[*client]bool),
 	}
 }
 
@@ -46,4 +46,22 @@ func (r *Room) SocketServe(c *gin.Context) {
 	if err != nil {
 		panic(err)
 	}
+
+	userCookie, err := c.Request.Cookie("auth")
+	if err != nil {
+		panic(err)
+	}
+
+	client := &client{
+		Socket: socket,
+		Send:   make(chan *message, types.MessageBufferSize),
+		Room:   r,
+		Name:   userCookie.Value,
+	}
+
+	r.Join <- client
+
+	defer func() {
+		r.Leave <- client
+	}()
 }
