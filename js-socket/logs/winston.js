0c6707920dc847bac12f42f795802f29bb69ef02
장연식 <sseregit@knowwheresoft.com>
2025-04-01 15:16:01 +0900

    판교 개발자의 bolierplate logger

--- js-socket/logs/winston.js
+++ js-socket/logs/winston.js
@@ -2,3 +2,38 @@ const {createLogger, format, transports} = require("winston");
 const winstonDaily = require("winston-daily-rotate-file");
 
 const logDir = "logs";
+
+const logFormat = format.printf((info) => {
+    return `${info.timestamp} ${info.level} : ${info.message}`;
+});
+
+const socketLogger = createLogger({
+    format: format.combine(
+        format.timestamp({
+            format: "YYYY-MM-DD HH:mm:ss"
+        }),
+        logFormat
+    ),
+    transports: [
+        new winstonDaily({
+            filename: "socket-info.log",
+            datePattern: "YYYY-MM-DD",
+            dirname: logDir + "/socket",
+            level: "info",
+        }),
+        new winstonDaily({
+            filename: "socket-error.log",
+            datePattern: "YYYY-MM-DD",
+            dirname: logDir + "/socket",
+            level: "error",
+        }),
+    ],
+});
+
+socketLogger.add(
+    new transports.Console({
+        format: format.combine(format.colorize(), format.simple()),
+    })
+);
+
+module.exports = {socketLogger}
